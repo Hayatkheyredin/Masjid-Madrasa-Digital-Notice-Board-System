07af7c718c1f39a7e994b5ea4e6eb5bc8a6c1b4a
Hayat Khayredin <hayu41630@gmail.com>
2026-04-15 00:27:01 +0300

    forgot password page

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { AuthPlaceholderPage } from './pages/AuthPlaceholderPage'
+import { ForgotPasswordPage } from './pages/ForgotPasswordPage'
 import { LandingPage } from './pages/LandingPage'
 import { SignInPage } from './pages/SignInPage'
 import { SignUpPage } from './pages/SignUpPage'
@@ -11,15 +11,7 @@ export function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="/login" element={<SignInPage />} />
-        <Route
-          path="/forgot-password"
-          element={
-            <AuthPlaceholderPage
-              title="Reset password"
-              description="We’ll add email reset flow when the API is ready."
-            />
-          }
-        />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
